47b4bbbfff945acd023c9618561bc7d214447c29
hananiasdaniel <hananiasdaniel@hotmail.com>
2018-10-22 18:03:35 -0300

    códigos finalizados

---
+++ Projeto1_Escalonadores/proj1.cpp
@@ -0,0 +1,194 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX 6 // maximo de elementos da fila
+#define QUANTUM 2
+
+typedef struct Processos
+{
+	int tChegada;
+	int tPico;
+	int tRestante; //para usar no RR
+} Processo;
+
+
+bool comparacao(Processo p1, Processo p2){
+	return (p1.tPico <= p2.tPico);
+}
+
+
+vector <Processo> LerArquivo(FILE *arq){
+	arq = fopen("numeros.txt", "rt");
+	int auxChegada; //variaveis auxiliares para 
+	int auxPico; 	//serem colocadas no struct
+
+	std::vector<Processo> p;
+
+	while(!feof(arq)){
+		fscanf(arq, "%d %d", &auxChegada, &auxPico);
+
+		if(feof(arq)) 
+			break;
+
+		Processo novo;
+		novo.tChegada = auxChegada;
+		novo.tPico = auxPico;
+		p.push_back(novo);
+	}
+
+	fclose(arq);
+
+
+	return p;
+
+}
+
+void FCFS(vector <Processo> filaProcessos){
+	float tRetorno = 0, tResposta = 0, tEspera = 0, tExec = 0;
+	int tamFila = filaProcessos.size(); //tamanho do vetor
+
+	for(int i = 0; i < filaProcessos.size(); i++){
+		Processo p = filaProcessos[i];
+		
+		if(p.tChegada > tExec){
+			tExec = p.tChegada;
+		}
+	
+		tRetorno += tExec + p.tPico - p.tChegada;
+		tResposta += tExec - p.tChegada;
+		tEspera += tExec - p.tChegada;
+	
+		tExec = tExec + p.tPico;
+	}
+
+	float mediaRetorno = (tRetorno/tamFila);
+	float mediaResposta = (tResposta/tamFila);
+	float mediaEspera = (tEspera/tamFila);
+
+	cout << "FCFS " << mediaRetorno << " " << mediaResposta << " " << mediaEspera << endl; 
+
+}
+
+void SJF(vector <Processo> filaProcessos){
+	float tRetorno = 0, tResposta = 0, tEspera = 0, tExec = 0;
+	int tamFila = filaProcessos.size(); //tamanho do vetor
+	int qProcesso = tamFila;
+
+	vector <Processo> procProntos; //lista dos processos prontos para execução
+
+	while(qProcesso != 0){
+		for(int i = 0; i < filaProcessos.size();  i++){
+			if(filaProcessos[i].tChegada <= tExec){
+				procProntos.push_back(filaProcessos[i]);
+				filaProcessos.erase(filaProcessos.begin()+i);
+				i--;
+			}
+		}
+		sort(procProntos.begin(), procProntos.end(), comparacao);
+		Processo p = procProntos[0];
+		qProcesso--;
+		procProntos.erase(procProntos.begin());
+
+		tEspera += tExec - p.tChegada;
+		tResposta += tExec - p.tChegada;
+		tExec += p.tPico;
+		tRetorno += tExec - p.tChegada;
+	
+
+	}
+
+	float mediaRetorno = (tRetorno/tamFila);
+	float mediaResposta = (tResposta/tamFila);
+	float mediaEspera = (tEspera/tamFila);
+
+	cout << "SJF " << mediaRetorno << " " << mediaResposta << " " << mediaEspera << endl; 
+
+}
+
+void RoundRobin(const vector <Processo> filaProcessos){ //FALHA DE SEGMENTAÇÃO
+	float tRetorno = 0, tResposta = 0, tEspera = 0, tExec = 0;
+	int tamFila = filaProcessos.size(); //tamanho do vetor
+	int qProcesso = tamFila;
+	bool voltaFila = false;
+
+	vector <Processo> procProntos;
+	vector <Processo> procNovos; //fila de novos processos
+	Processo p;
+	p.tPico = 0;
+	p.tChegada = 0;
+
+	for(int i = 0; i < filaProcessos.size(); i++){
+		procNovos.push_back(filaProcessos[i]);;
+	}
+
+	while(qProcesso != 0){
+		for(int i = 0; i < procProntos.size(); i++){
+			if(procNovos[i].tChegada == tExec){
+				procProntos.push_back(procNovos[i]);
+				procNovos.erase(procNovos.begin()+i);
+				i--;
+				
+			}else if(procNovos[i].tChegada < tExec){
+				tEspera += tExec - procNovos[i].tChegada;
+				procProntos.push_back(procNovos[i]);
+				procNovos.erase(procNovos.begin()+i);
+				i--;
+			}
+		}
+
+		if(voltaFila){
+			procProntos.push_back(p);
+			voltaFila = false;
+		}
+
+		p = procProntos[0];
+		procProntos.erase(procProntos.begin());
+
+		if(p.tRestante == p.tPico){
+			tResposta += tExec - p.tChegada;
+		}
+
+		tExec += QUANTUM;
+		p.tRestante -= QUANTUM;
+		tEspera += QUANTUM * procProntos.size();
+
+
+		if(p.tRestante > 0){
+			voltaFila = true;
+		}
+
+		if(p.tRestante <= 0){
+			qProcesso--;
+			tExec += p.tRestante;
+			tEspera += (p.tRestante * int(procProntos.size()));
+			tRetorno += tExec - p.tChegada;
+		}
+
+	}
+
+	float mediaRetorno = (tRetorno/tamFila);
+	float mediaResposta = (tResposta/tamFila);
+	float mediaEspera = (tEspera/tamFila);
+
+	cout << "RR " << mediaRetorno << " " << mediaResposta << " " << mediaEspera << endl; 
+
+}
+
+
+int main(){
+	FILE *arq;
+	vector <Processo> filaProcessos;
+	int tamFila = filaProcessos.size();
+
+	filaProcessos = LerArquivo(arq);
+	FCFS(filaProcessos);
+	SJF(filaProcessos);
+	RoundRobin(filaProcessos);
+
+	return 0;
+
+}
---
+++ Projeto2_Paginacao/proj2.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+//vector <int> listaPagina;
+int n_quadros; 
+
+bool BuscaMemo(int *ar, int pagina){
+	for(int i = 0; i < n_quadros; i++){
+		if(pagina == ar[i]){
+			return true;
+		}
+	}
+	return false;
+}
+
+int BuscaMemo1(vector <int> ar, int pagina){ //usado para o OTM
+	for(int i = 0; i < n_quadros; i++){
+		if(pagina == ar[i]){
+			return i;
+		}
+	}
+	return -1; //se retornar -1 significa que a pagina nao esta na memoria
+}
+
+
+vector <int> LerArquivo(FILE *arq){
+	arq = fopen("entrada2.txt", "rt");
+	int pAtual;
+
+	vector<int> p;
+
+	while(!feof(arq)){
+		fscanf(arq, "%d", &pAtual);
+
+		if(feof(arq)) 
+			break;
+
+		p.push_back(pAtual);
+	}
+
+	fclose(arq);
+
+	n_quadros = p[0];
+	p.erase(p.begin());
+
+
+	return p;
+
+}
+
+void FIFO(vector<int> listaPagina){
+	int faltaPag = 0;
+	int nPagina = listaPagina.size();
+	int memo[n_quadros] = {};
+	int contador = 0;
+	int z = 0;
+
+	if(nPagina == 0){
+		cout << "Nao existem paginas" << endl;
+	}
+
+	for(int i = 0; i < nPagina; i++){
+		if(!BuscaMemo(memo, listaPagina[i])){
+			if(contador == n_quadros){
+				contador = 0;
+			}
+			memo[contador] = listaPagina[i];
+			faltaPag++;
+			contador++; 
+		}	
+	}
+	cout << "FIFO" << " " << faltaPag << endl;
+
+}
+
+void OTM(vector <int> listaPagina){
+	int faltaPag = 0;
+	int nPagina = listaPagina.size(); //tamanho da lista
+	int memo[n_quadros] = {}; //armazena os valores que estao nos quadros
+	int contador = 0;
+	int i = 0;
+
+	if(nPagina == 0){
+		cout << "Nao existem paginas" << endl;
+	}
+
+	for(i = 0; i < nPagina; i++){
+		if(faltaPag < n_quadros && !BuscaMemo(memo, listaPagina[i])){
+			memo[faltaPag] = listaPagina[i];
+			faltaPag++;
+			continue;
+		}else if(!BuscaMemo(memo, listaPagina[i])){
+			vector <int> aux;
+
+			for(int k = 0; k < n_quadros; k++){
+				aux.push_back(memo[k]);
+			}
+
+			for(int j = i; j < nPagina; j++){
+				int pos = BuscaMemo1(aux, listaPagina[j]);
+				if(pos >= 0 && aux.size() > 1){
+					aux.erase(aux.begin()+pos);
+				}
+			}
+
+			int pagRemoved = aux[0];
+			for(int l = 0; l < n_quadros; l++){
+				if(memo[l] == pagRemoved){
+					memo[l] = listaPagina[i];
+				}
+			}
+			faltaPag++;
+		}
+	}
+	
+	cout << "OTM" << " " << faltaPag << endl;
+
+}
+
+void LRU(vector <int> listaPagina){
+	int faltaPag = 0;
+	int nPagina = listaPagina.size();
+	int memo[n_quadros] = {};
+	int contador = 0;
+	int i;
+
+	if(nPagina == 0){
+		cout << "Nao existem paginas" << endl;
+	}
+
+	for(i = 0; i < nPagina; i++){
+		if(faltaPag < n_quadros && !BuscaMemo(memo, listaPagina[i])){
+			memo[faltaPag] = listaPagina[i];
+			faltaPag++;
+			continue;
+		}else if(!BuscaMemo(memo, listaPagina[i])){
+			vector <int> aux;
+
+			for(int k = 0; k < n_quadros; k++){
+				aux.push_back(memo[k]);
+			}
+
+			for(int j = i; j > 0; j--){
+				int pos = BuscaMemo1(aux, listaPagina[j]);
+				if(pos >= 0 && aux.size() > 1){
+					aux.erase(aux.begin()+pos);
+				}
+			}
+
+			int pagRemoved = aux[0];
+			for(int l = 0; l < n_quadros; l++){
+				if(memo[l] == pagRemoved){
+					memo[l] = listaPagina[i];
+				}
+			}
+			faltaPag++;
+		}
+	}
+
+	cout << "LRU" << " " << faltaPag << endl;
+
+}
+
+
+
+int main(){
+	FILE *arq;
+
+	vector <int> AlgoritmoFIFO;
+	vector <int> AlgoritmoOtimo;
+	vector <int> AlgoritmoLRU;
+	
+
+	AlgoritmoFIFO = LerArquivo(arq);
+	AlgoritmoOtimo = LerArquivo(arq);
+	AlgoritmoLRU = LerArquivo(arq);
+	
+
+	FIFO(AlgoritmoFIFO);
+	OTM(AlgoritmoOtimo);
+	LRU(AlgoritmoLRU);
+
+	return 0;
+	
+}
